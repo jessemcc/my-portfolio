01e38e9072aefead18f6e2913353b042abd2209f
jessemcc <jesse.mcconnell@live.ca>
2023-02-23 11:19:09 -0500

    Added initial styles, fonts, pages, components and assets

---
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -0,0 +1,11 @@
+import "./ProjectsPage.scss";
+
+const ProjectsPage = () => {
+  return (
+    <section className="projects">
+      <article className="projects__container"></article>
+    </section>
+  );
+};
+
+export default ProjectsPage;
