4d4617fae5ee66fc047b4fa0811a9573ec5ae042
jessemcc <jesse.mcconnell@live.ca>
2023-02-24 16:08:07 -0500

    completed navbar using bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <section className="app">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
