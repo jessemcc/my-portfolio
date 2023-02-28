710ed176fce47f823688362d6bef8b9269ee2fad
jessemcc <jesse.mcconnell@live.ca>
2023-02-28 17:36:36 -0500

    Completed hero section for all sizes

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,12 @@
 import "./HomePage.scss";
+import Hero from "../../components/Hero/Hero";
 
 const HomePage = () => {
   return (
     <section className="home">
-      <article className="home__container"></article>
+      <article className="home__container">
+        <Hero />
+      </article>
     </section>
   );
 };
